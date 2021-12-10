fc9fa36ecce7141a4ab82454960f3b000c0082bb
Odd Mathisen <oddoysteim@uia.no>
2021-12-10 15:16:15 +0100

    :adhesive_bandage: Patch: Fixed copy method

--- generate_folders.py
+++ generate_folders.py
@@ -1,4 +1,4 @@
-import os
+import shutil
 from files_renamer import rename_files
 
 
@@ -13,8 +13,7 @@ def main(placeholder, start_day, end_day, template_dir):
     # the placeholder in the new directory to the day number
     for day_num in range(start_day, end_day + 1):
         new_dir = template_dir.replace(placeholder, str(day_num))
-        os.mkdir(new_dir)
-        os.system("cp -r " + template_dir + "/* " + new_dir)
+        shutil.copytree(template_dir, new_dir)
         rename_files(new_dir, placeholder, str(day_num))
 
 
