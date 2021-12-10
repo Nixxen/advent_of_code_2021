652077cb541e53e16bf687b2c717037489ad88ac
Odd Mathisen <oddoysteim@uia.no>
2021-12-10 15:40:50 +0100

    :art: Structure: Pad days under 10 with a leading 0.
    Quick fix in generate folders to include leading 0 for low numbers

--- generate_folders.py
+++ generate_folders.py
@@ -11,8 +11,12 @@ TEMPLATE_DIR = "day XX"
 def main(placeholder, start_day, end_day, template_dir):
     # Make a copy of the template directory and its contents and rename
     # the placeholder in the new directory to the day number
+    # Pads the day number with a leading zero if necessary
     for day_num in range(start_day, end_day + 1):
-        new_dir = template_dir.replace(placeholder, str(day_num))
+        day_str = str(day_num)
+        if len(day_str) == 1:
+            day_str = "0" + day_str
+        new_dir = template_dir.replace(placeholder, day_str)
         shutil.copytree(template_dir, new_dir)
         rename_files(new_dir, placeholder, str(day_num))
 
