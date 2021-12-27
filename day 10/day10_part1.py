8d389d3378888076444b5ae0d09cb0f12d521021
Odd Mathisen <oddoysteim@uia.no>
2021-12-27 01:47:55 +0100

    :sparkles: New feature: Day 10 completed

--- day 10/day10_part1.py
+++ day 10/day10_part1.py
@@ -1,26 +1,89 @@
-RUN_TEST = True
-TEST_SOLUTION = ...
-TEST_INPUT_FILE = 'test_input_day_10.txt'
-INPUT_FILE = 'input_day_10.txt'
+RUN_TEST = False
+TEST_SOLUTION = 26397
+TEST_INPUT_FILE = "test_input_day_10.txt"
+INPUT_FILE = "input_day_10.txt"
 
 ARGS = []
 
 
-def main_part1(input_file, ):
+from collections import deque
+
+
+def is_open_bracket(char):
+    return char in ["(", "{", "<", "["]
+
+
+def is_close_bracket(char):
+    return char in [")", "}", ">", "]"]
+
+
+def is_matching_bracket(open_bracket, close_bracket):
+    return (
+        (open_bracket == "(" and close_bracket == ")")
+        or (open_bracket == "{" and close_bracket == "}")
+        or (open_bracket == "[" and close_bracket == "]")
+        or (open_bracket == "<" and close_bracket == ">")
+    )
+
+
+def main_part1(
+    input_file,
+):
     with open(input_file) as file:
         lines = list(map(lambda line: line.rstrip(), file.readlines()))
 
-    ...
+    # Day 10 Part 1 - Bracket matching. Some lines are incomplete, but others
+    # are corrupted. Find and discard the corrupted lines first. A corrupted
+    # line is one where a chunk closes with the wrong character - that is,
+    # where the characters it opens and closes with do not form one of the four
+    # legal pairs [], {}, () or <>.
 
-    solution = ...
+    # To calculate the syntax error score for a line, take the first illegal
+    # character on the line and look it up in the following table:
+    # ): 3 points.
+    # ]: 57 points.
+    # }: 1197 points.
+    # >: 25137 points.
+    # The total syntax error score for the entire subsystem is the sum of the
+    # scores for each line.
+
+    # Find the first illegal character in each corrupted line of the navigation
+    # subsystem. What is the total syntax error score for those errors?
+
+    score_table = {
+        ")": 3,
+        "]": 57,
+        "}": 1197,
+        ">": 25137,
+    }
+
+    score_list = []
+    for line in lines:
+        syntax_stack = deque()
+        for char in line:
+            if is_open_bracket(char):
+                syntax_stack.append(char)
+            elif is_close_bracket(char):
+                if not syntax_stack:
+                    score_list.append(char)
+                    break
+                else:
+                    open_bracket = syntax_stack.pop()
+                    if not is_matching_bracket(open_bracket, char):
+                        score_list.append(char)
+                        break
+
+    # The total syntax error score for the navigation subsystem is the sum of
+    # the scores for each line.
+    solution = sum(map(lambda char: score_table[char], score_list))
     return solution
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if RUN_TEST:
         solution = main_part1(TEST_INPUT_FILE, *ARGS)
         print(solution)
-        assert (TEST_SOLUTION == solution)
+        assert TEST_SOLUTION == solution
     else:
         solution = main_part1(INPUT_FILE, *ARGS)
         print(solution)
--- day 10/day10_part2.py
+++ day 10/day10_part2.py
@@ -1,26 +1,92 @@
-RUN_TEST = True
-TEST_SOLUTION = ...
-TEST_INPUT_FILE = 'test_input_day_10.txt'
-INPUT_FILE = 'input_day_10.txt'
+RUN_TEST = False
+TEST_SOLUTION = 288957
+TEST_INPUT_FILE = "test_input_day_10.txt"
+INPUT_FILE = "input_day_10.txt"
 
 ARGS = []
 
+from collections import deque
+from day10_part1 import is_close_bracket, is_open_bracket, is_matching_bracket
 
-def main_part2(input_file, ):
+
+def get_correct_closing_bracket(open_bracket):
+    if open_bracket == "(":
+        return ")"
+    elif open_bracket == "[":
+        return "]"
+    elif open_bracket == "{":
+        return "}"
+    elif open_bracket == "<":
+        return ">"
+
+
+def main_part2(
+    input_file,
+):
     with open(input_file) as file:
         lines = list(map(lambda line: line.rstrip(), file.readlines()))
 
-    ...
+    # Day 10 Part 2 - Bracket matching fixing. Incomplete lines don't have any
+    # incorrect characters - instead, they're missing some closing characters
+    # at the end of the line. To repair the navigation subsystem, you just need
+    # to figure out the sequence of closing characters that complete all open
+    # chunks in the line. You can only use closing characters; ), ], }, or >,
+    # and you must add them in the correct order so that only legal pairs are
+    # formed and all chunks end up closed.
 
-    solution = ...
+    # The score is determined by considering the completion string
+    # character-by-character. Start with a total score of 0. Then, for each
+    # character, multiply the total score by 5 and then increase the total
+    # score by the point value given for the character in the following table:
+    # ): 1 point.
+    # ]: 2 points.
+    # }: 3 points.
+    # >: 4 points.
+
+    score_table = {
+        ")": 1,
+        "]": 2,
+        "}": 3,
+        ">": 4,
+    }
+
+    # The winner is found by sorting all of the scores and then taking the
+    # middle score. Find the completion string for each incomplete line, score
+    # the completion strings, and sort the scores. What is the middle score?
+
+    score_list = []
+    for line in lines:
+        line_score_list = []
+        syntax_stack = deque()
+        for char in line:
+            if is_open_bracket(char):
+                syntax_stack.append(char)
+            elif is_close_bracket(char):
+                if not syntax_stack:
+                    print(f"Error: {char} without matching open bracket")
+                    break
+                else:
+                    open_bracket = syntax_stack.pop()
+                    if not is_matching_bracket(open_bracket, char):
+                        break  # Skip corrupted lines
+        else:  # No break in the for loop
+            while syntax_stack:
+                missing_bracket = syntax_stack.pop()
+                line_score_list.append(get_correct_closing_bracket(missing_bracket))
+            score = 0
+            for i, bracket in enumerate(line_score_list):
+                score = score * 5 + score_table[bracket]
+            score_list.append(score)
+
+    solution = sorted(score_list)[len(score_list) // 2]
     return solution
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if RUN_TEST:
         solution = main_part2(TEST_INPUT_FILE, *ARGS)
         print(solution)
-        assert (TEST_SOLUTION == solution)
+        assert TEST_SOLUTION == solution
     else:
         solution = main_part2(INPUT_FILE, *ARGS)
         print(solution)
